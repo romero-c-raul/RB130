f29cef4bdf4fd2cf1d82e7398ca7de20b51047f0
Raul Romero <raulromerocano@aggienetwork.com>
2020-12-15 20:14:32 +0000

    Added functionality to interrogate the list

--- lesson_1_blocks/todo_list.rb
+++ lesson_1_blocks/todo_list.rb
@@ -43,7 +43,7 @@ end
 # on a TodoList object, including iteration and selection.
 
 class TodoList
-  attr_accessor :title, :todos
+  attr_accessor :title
 
   def initialize(title)
     @title = title
@@ -56,6 +56,33 @@ class TodoList
     raise TypeError, "Can only add Todo objects" unless other_list.class == Todo
     @todos << other_list 
   end
+  
+  def size
+    todos.size
+  end
+  
+  def first
+    todos.first.title
+  end
+  
+  def last
+    todos.last.title
+  end
+  
+  def to_a
+    todos.map do |current_todo|
+      current_todo.title
+    end
+  end
+  
+  def done?
+    todos.all? do |current_todo|
+      current_todo.done == true
+    end
+  end
+  
+  protected
+  attr_accessor :todos
 
 end
 
@@ -75,3 +102,20 @@ list.add(todo3)                 # adds todo3 to end of list, returns list
 
 # <<
 # same behavior as add
+
+# ---- Interrogating the list -----
+
+# size
+p list.size                       # returns 3
+
+# first
+p list.first                      # returns todo1, which is the first item in the list
+
+# last
+p list.last                       # returns todo3, which is the last item in the list
+
+#to_a
+p list.to_a                      # returns an array of all items in the list
+
+#done?
+p list.done?                     # returns true if all todos in the list are done, otherwise false
