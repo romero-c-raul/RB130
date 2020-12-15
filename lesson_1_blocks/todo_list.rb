b33922c03433bb3a679514a9a7928684c0229734
Raul Romero <raulromerocano@aggienetwork.com>
2020-12-15 20:06:55 +0000

    I'mplemented adding to list functionality

---
+++ lesson_1_blocks/todo_list.rb
@@ -0,0 +1,77 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title, :todos
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  # rest of class needs implementation
+  
+  def add(other_list)
+    raise TypeError, "Can only add Todo objects" unless other_list.class == Todo
+    @todos << other_list 
+  end
+
+end
+
+# given
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)                 # adds todo1 to end of list, returns list
+list.add(todo2)                 # adds todo2 to end of list, returns list
+list.add(todo3)                 # adds todo3 to end of list, returns list
+#list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+
+# <<
+# same behavior as add
